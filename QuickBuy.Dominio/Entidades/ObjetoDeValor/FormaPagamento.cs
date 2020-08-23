e1e3cdd22d7b016c4d2a4c45ef0887615de86191
A598168 <A598168@DESKTOP-0OE3BUE>
2020-08-23 20:16:23 -0300

    Criacao Entidades

---
+++ QuickBuy.Dominio/Entidades/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,35 @@
+﻿using QuickBuy.Dominio.Entidades.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public bool EhBoleto
+        {
+
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+
+        public bool NaoFoiDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+
+        public bool EhCartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+
+        public bool EhDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+        }     
+    }
+}
